e7ffe98d384fdaadb68d390a71c40946cdf090dc
Kodi Bharadwaj <138383233+KodiBharadwaj@users.noreply.github.com>
2023-07-27 09:58:28 +0530

    Update messageControllers.js

--- controllers/messageControllers.js
+++ controllers/messageControllers.js
@@ -3,6 +3,18 @@ const Message=require("../models/messageModel");
 const User = require("../models/userModel");
 const Chat = require("../models/chatModel");
 
+const allMessages = asyncHandler(async (req, res) => {
+  try {
+    const messages = await Message.find({ chat: req.params.chatId })
+      .populate("sender", "name pic email")
+      .populate("chat");
+    res.json(messages);
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
+});
+
 const sendMessage=asyncHandler(async (req,res)=>{
     const { content, chatId } = req.body;
 
@@ -18,8 +30,8 @@ const sendMessage=asyncHandler(async (req,res)=>{
   try {
     var message = await Message.create(newMessage);
 
-    message = await message.populate("sender", "name pic");
-    message = await message.populate("chat"); 
+    message = await message.populate("sender", "name pic").execPopulate();
+    message = await message.populate("chat").execPopulate(); 
     message = await User.populate(message, {
       path: "chat.users",
       select: "name pic email",
@@ -35,16 +47,5 @@ const sendMessage=asyncHandler(async (req,res)=>{
 
 });
 
-const allMessages = asyncHandler(async (req, res) => {
-  try {
-    const messages = await Message.find({ chat: req.params.chatId })
-      .populate("sender", "name pic email")
-      .populate("chat");
-    res.json(messages);
-  } catch (error) {
-    res.status(400);
-    throw new Error(error.message);
-  }
-});
 
-module.exports={sendMessage, allMessages};
+module.exports={allMessages, sendMessage};
