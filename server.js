17cd076acb5b9307465962c74e99813ef0744f6c
Kodi Bharadwaj <138383233+KodiBharadwaj@users.noreply.github.com>
2023-07-28 12:46:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ const server = app.listen(5000,console.log(`Server started on PORT ${PORT}`.yell
 const io = require("socket.io")(server, {
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://mern-messenger.onrender.com",
     // credentials: true,
   },
 });
